91d696c91bd07de12ffee402f65780061c55f629
Allison Serrano <allisonserrano171@yahoo.com>
2023-08-06 15:12:57 -0400

    finished product routes, starting tag routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -112,8 +112,22 @@ router.put('/:id', async (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const product = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!product) {
+      res.status(404).json({ message: 'No user with this id!' });
+      return;
+    }
+    res.status(200).json(product);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,10 +3,17 @@ const sequelize = require('../../config/connection');
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
-
-router.get('/', (req, res) => {
-  // find all tags
+ // find all tags
   // be sure to include its associated Product data
+router.get('/', async (req, res) => {
+  try {
+    const tag = await Tag.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(tag);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
